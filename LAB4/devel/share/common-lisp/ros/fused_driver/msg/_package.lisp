1446eddce9c01847ef35b573729722c6de4bc5a0
annevinesh <anne.v@northeastern.edu>
2023-03-26 02:55:30 -0400

    update

--- LAB4/devel/share/common-lisp/ros/fused_driver/msg/_package.lisp
+++
@@ -1,9 +0,0 @@
-(cl:defpackage fused_driver-msg
-  (:use )
-  (:export
-   "<VECTORNAV>"
-   "VECTORNAV"
-   "<GPS_MSG>"
-   "GPS_MSG"
-  ))
-
--- LAB4/devel/share/fused_driver/cmake/fused_driver-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(fused_driver_MESSAGE_FILES "/home/cracker/EECE5554/LAB4/src/fused_driver/msg/gps_msg.msg;/home/cracker/EECE5554/LAB4/src/fused_driver/msg/Vectornav.msg")
-set(fused_driver_SERVICE_FILES "")
